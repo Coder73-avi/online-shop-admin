cb503b9f2578eadfa6217c040c25cee1c7a5ba51
hunter <hunter@localhost.localdomain>
2022-11-21 14:45:40 +0545

    Change the axios url

--- controller/axios.js
+++ controller/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 let URL;
 URL = process.env.URL || "http://localhost:4001";
-URL = "http://localhost:4001";
+// URL = "http://localhost:4001";
 const instance = axios.create({
   baseURL: URL, // THE API (cloud function) URL
   withCredentials: true,
